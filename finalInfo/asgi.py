d0bac878b7c3f63369a99b29a7f7d408f9f0e210
yuliEsc <yuli_07@hotmail.com.ar>
2022-09-10 18:28:07 -0300

    seis

--- finalInfo/asgi.py
+++ finalInfo/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finalInfo.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finalInfo.settings.production')
 
 application = get_asgi_application()
--- finalInfo/wsgi.py
+++ finalInfo/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyectofinalInfo.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyectofinalInfo.settings.production')
 
 application = get_wsgi_application()
 application = WhiteNoise(application)
